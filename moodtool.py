632bfaf764073c264be72bad15bb9080fd37fa56
colinyao <colinyao@uchicago.edu>
2025-05-18 14:08:45 -0500

    setup

--- moodtool.py
+++ moodtool.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import google.auth as ga
 
+# Magic constants here
+sheet_id = "1rs37GT6bir0W5Qs9Wqd51Dd5ZJlulnTGmTGfUk5RzfA"
+
 
 st.title("Placeholder title")
 
 st.markdown("Placeholder text")
 
 if st.button("button1"):
-    st.balloons
+    st.write("apples")
 
 if st.button("button2"):
-    st.balloons
+    st.write("bananas")
