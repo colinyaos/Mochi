3c5ed4e5518a8c6706d6a4fb2c8f8b92a8e70d45
colinyao <colinyao@uchicago.edu>
2025-05-18 13:56:33 -0500

    placeholders

--- moodtool.py
+++ moodtool.py
@@ -1,2 +1,13 @@
 import streamlit as st
 import google.auth as ga
+
+
+st.title("Placeholder title")
+
+st.markdown("Placeholder text")
+
+if st.button("button1"):
+    st.balloons
+
+if st.button("button2"):
+    st.balloons
