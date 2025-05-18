81c191872c380e5152dfd10c37cb84afac692547
colinyao <colinyao@uchicago.edu>
2025-05-18 14:17:13 -0500

    google hooks

--- moodtool.py
+++ moodtool.py
@@ -1,16 +1,57 @@
 import streamlit as st
 import google.auth as ga
+from googleapiclient.discovery import build
 
 # Magic constants here
 sheet_id = "1rs37GT6bir0W5Qs9Wqd51Dd5ZJlulnTGmTGfUk5RzfA"
 
+mood_send = ""
+
+
+# goog api sender
+def send_data(mood):
+    creds, _ = ga.default()
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        values = [
+            [
+                # Cell values ...
+            ],
+            # Additional rows ...
+        ]
+        body = {"values": values}
+        result = (
+            service.spreadsheets()
+            .values()
+            .append(
+                spreadsheetId=sheet_id,
+                range=range_name,
+                valueInputOption=value_input_option,
+                body=body,
+            )
+            .execute()
+        )
+        print(f"{(result.get('updates').get('updatedCells'))} cells appended.")
+        return result
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+
+# st page below
 
 st.title("Placeholder title")
 
 st.markdown("Placeholder text")
 
 if st.button("button1"):
-    st.write("apples")
+    mood_send = "happy"
 
 if st.button("button2"):
-    st.write("bananas")
+    mood_send = "sad"
+
+if st.button("submit button"):
+    st.write(mood_send)
+
